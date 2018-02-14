3ef877377ce3feedfccc8bd58026d358ea5e8d55
SAMANTHA\Ed <edmond.ipi@gmail.com>
2018-02-14 15:15:03 +0000

    SURF functionality.

--- surf_flann.py
+++ surf_flann.py
@@ -7,12 +7,13 @@ img1 = cv2.imread('b_crop.png')
 img2 = cv2.imread('bottle.png')
 gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-sift = cv2.xfeatures2d.SIFT_create()
+surf = cv2.xfeatures2d.SURF_create(400)
 
 time_before = time.time()
-kp1, des1 = sift.detectAndCompute(gray1, None)
-kp2, des2 = sift.detectAndCompute(gray2, None)
+kp1, des1 = surf.detectAndCompute(gray1, None)
+kp2, des2 = surf.detectAndCompute(gray2, None)
 time_after = time.time()
+
 
 print('Time:', (time_after-time_before))
 
@@ -20,6 +21,7 @@ print('Time:', (time_after-time_before))
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks=50)   # or pass empty dictionary
+
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
 matches = flann.knnMatch(des1,des2,k=2)
@@ -41,7 +43,6 @@ draw_params = dict(matchColor = (0,255,0),
                    singlePointColor = (255,0,0),
                    matchesMask = matchesMask,
                    flags = 0)
-
 img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 
 plt.axis('off')
